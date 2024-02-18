a6a061220bca8b4e4b18911affe69fe1d2efe1a0
Bryan Tung <bryantung89@gmail.com>
2024-02-18 23:15:26 +0800

    fix static params

--- app/reports/[year]/[month]/page.tsx
+++ app/reports/[year]/[month]/page.tsx
@@ -1,3 +1,17 @@
+export async function generateStaticParams() {
+  return [
+    {year: "2023", month: "May"},
+    {year: "2023", month: "June"},
+    {year: "2023", month: "July"},
+    {year: "2023", month: "August"},
+    {year: "2023", month: "September"},
+    {year: "2023", month: "October"},
+    {year: "2023", month: "November"},
+    {year: "2023", month: "December"},
+    {year: "2024", month: "January"},
+    {year: "2024", month: "February"},
+  ]
+}
 
 export default function MonthlyReports({ params }: { params: { year: string, month: string } }) {
 
