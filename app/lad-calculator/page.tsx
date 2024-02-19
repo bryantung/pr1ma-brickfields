58984088af2805d1cf41bd2ce386f031ecdee00b
Bryan Tung <bryantung89@gmail.com>
2024-02-20 00:44:46 +0800

    added option to include/exclude mco period for common facilities lad

--- app/lad-calculator/page.tsx
+++ app/lad-calculator/page.tsx
@@ -16,6 +16,7 @@ export default function LADCalculator() {
   const [showMCOOffset, setShowMCOOffset] = useState<boolean>(true);
   const [ladUnitAmount, setLADUnitAmount] = useState<number>(0);
   const [ladCFAmount, setLADCFAmount] = useState<number>(0);
+  const [mcoOffsetLAD, setMCOOffsetLAD] = useState<boolean>(false);
 
   const getDelayInDays = (withMCO: boolean): number => {
     const spaDeliveryDate = moment(spaDate).add(36, "month");
@@ -30,12 +31,12 @@ export default function LADCalculator() {
     }
 
     const getCFLADAmount = (): number => {
-      return (cfLADPercentage * purchaseAmount * unitLADPercentage * getDelayInDays(true)) / 365;
+      return (cfLADPercentage * purchaseAmount * unitLADPercentage * getDelayInDays(mcoOffsetLAD)) / 365;
     }
 
     setLADUnitAmount(getUnitLADAmount());
     setLADCFAmount(getCFLADAmount());
-  }, [spaDate, estimatedVP, purchaseAmount, getDelayInDays, setLADUnitAmount, setLADCFAmount]);
+  }, [spaDate, estimatedVP, purchaseAmount, mcoOffsetLAD, getDelayInDays, setLADUnitAmount, setLADCFAmount]);
 
   return (
     <main className="flex flex-col grow container mx-auto px-8 pb-16 md:px-24 md:pb-36">
@@ -48,7 +49,17 @@ export default function LADCalculator() {
         <div className="border border-brickfields dark:border-white" />
         <div>365</div>
       </div>
-      <div className="calculator mt-8">
+      <hr className="my-8 border-brickfields dark:border-inherit" />
+      <p>According to Clause 28(2), owners may claim LAD for common facilities, below is the calculation formular:</p>
+      <br />
+      <div className="inline-block text-center self-center">
+        <div>20% x harga rumah x 10% x [jumlah hari tertunggak - tempoh pengecualian*]</div>
+        <div className="border border-brickfields dark:border-white" />
+        <div>365</div>
+        <sub>* It is not clear that Covid-19 Act exemption period is applicable to common facilities</sub>
+      </div>
+      <hr className="my-8 border-brickfields dark:border-inherit" />
+      <div className="calculator">
         <p className="mb-4">You can utilize the calculator below to calculate the estimated LAD amount.<sup>2</sup></p>
         <div className="grid grid-cols-4 gap-3 items-center md:w-3/4 mx-auto">
           <label htmlFor="spa-date">SPA Signed Date</label>
@@ -80,9 +91,9 @@ export default function LADCalculator() {
           <label>Days of Delay</label>
           <div className="grid grid-cols-2 col-span-3">
             <span className="text-red-500">{getDelayInDays(showMCOOffset)} Days</span>
-            <label htmlFor="mco-offset">
+            <label htmlFor="mco-offset-delay">
               <input
-                id="mco-offset"
+                id="mco-offset-delay"
                 className="mr-2"
                 type="checkbox"
                 defaultChecked={showMCOOffset}
@@ -93,9 +104,21 @@ export default function LADCalculator() {
           </div>
           <div className="col-span-4 text-center font-bold">Estimated LAD Amount</div>
           <div className="col-span-2 text-right">Unit</div>
-          <div className="col-span-2 text-green-700">RM {ladUnitAmount.toFixed(2)}</div>
+          <div className="col-span-2 text-green-600 drop-shadow-xl">RM {ladUnitAmount.toFixed(2)}</div>
           <div className="col-span-2 text-right">Common Facilities</div>
           <div className="col-span-2 text-yellow-500">RM {ladCFAmount.toFixed(2)}</div>
+          <div className="grid grids-cols-4 col-start-3">
+          <label htmlFor="mco-offset-lad">
+              <input
+                id="mco-offset-lad"
+                className="mr-2"
+                type="checkbox"
+                defaultChecked={mcoOffsetLAD}
+                onChange={(event: ChangeEvent<HTMLInputElement>) => setMCOOffsetLAD(event.target.checked)}
+              />
+              Offset MCO Period
+            </label>
+          </div>
           <div className="col-span-2 text-right font-bold">Total</div>
           <div className="col-span-2 text-green-400">RM {(ladUnitAmount + ladCFAmount).toFixed(2)}</div>
         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -25,16 +25,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={layoutClasses}>
-        <h1 className={`text-center text-4xl py-6 shadow-md sticky top-0 grow-0 mb-6 bg-white dark:bg-brickfields`}>
-          Residensi PR1MA Brickfields <sup className="text-sm -top-4">1</sup>
-        </h1>
+        <header className={`py-6 shadow-md sticky top-0 grow-0 mb-6 bg-white dark:bg-brickfields z-10`}>
+          <h1 className={`text-center text-4xl`}>
+            Residensi PR1MA Brickfields <sup className="text-sm -top-4">1</sup>
+          </h1>
+        </header>
         {children}
-        <footer className="bg-pink-600 min-h-36 grow-0 p-8 md:px-24">
+        <footer className="bg-brickfields dark:bg-pink-600 text-white min-h-36 grow-0 p-8 md:px-24">
           <div className="text-sm">
             Disclaimers:<br />
             <sup>[1]</sup> This is an unoffical page of Residensi PR1MA Brickfields, solely created by one of the homeowner for sharing information.
             <br />
-            <sup>[2]</sup> The LAD Calculator is only meant for reference purposes, it doesn't hold any legal authority against the actual compensation presented by PR1MA.
+            <sup>[2]</sup> The LAD Calculator is only for reference purposes, it doesn't hold any legal authority against the actual compensation presented by PR1MA.
           </div>
         </footer>
       </body>
